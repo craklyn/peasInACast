c8a129e0239606b2cbb5ed5b4697762c83b07bb2
Daniel Blackburn <craklyn@gmail.com>
2017-07-15 13:41:37 -0700

    proof of concept for getting transcript given file

---
+++ transcribe/apiCall.sh
@@ -0,0 +1,4 @@
+curl -X POST --form "file=@elevatorPitch.mp3" --form "apikey=ab00903b-664d-4efa-9966-4c258c562145" https://api.havenondemand.com/1/api/async/recognizespeech/v1
+
+curl -X GET https://api.havenondemand.com/1/job/status/w-eu_8bb2767c-0e10-4db3-a97f-f67e9c8edd20?apikey=ab00903b-664d-4efa-9966-4c258c562145
+
