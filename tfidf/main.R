8d4c462ed18978b19b8a91cd7ccc46190edfc62d
Daniel Blackburn <craklyn@gmail.com>
2017-07-15 15:06:51 -0700

    Modified code to handle multiple files

--- tfidf/main.R
+++ tfidf/main.R
@@ -10,11 +10,12 @@ library(ggplot2)
 library(RColorBrewer)
 library(wordcloud)
 
-podcastData <- c(paste(readLines('Data/620.txt'), collapse=' '))
+episodes <- list.files("Data")
+podcastData <- sapply(episodes, function(x) paste(readLines(paste0("Data/",x), encoding="UTF-8"), collapse=' '))
 
 set.seed(1300)
 
-corpus <- VCorpus(VectorSource(podcastData)) %>%
+corpus <- VCorpus(VectorSource(podcastData[9])) %>%
   tm_map(x = ., FUN = PlainTextDocument) %>%
   tm_map(x = ., FUN = removePunctuation) %>%
   tm_map(x = ., FUN = removeNumbers) %>%
@@ -22,7 +23,7 @@ corpus <- VCorpus(VectorSource(podcastData)) %>%
   tm_map(x = ., FUN = stripWhitespace)
 
 doc_term <- DocumentTermMatrix(corpus)
-doc_term$dimnames$Docs <- "episode 620"
+doc_term$dimnames$Docs <- c("episode 620", "episode 109")
 
 tf_idf <- weightTfIdf(m = doc_term, normalize = TRUE)
 tf_idf_mat <- as.matrix(tf_idf)
