2241e06cb2105b2cfbe877b8661f1768a4fb5a66
Daniel Blackburn <craklyn@gmail.com>
2017-07-15 23:43:14 -0700

    Major bug fixes to data process

--- tfidf/main.R
+++ tfidf/main.R
@@ -10,17 +10,11 @@ library(ggplot2)
 library(RColorBrewer)
 library(wordcloud)
 
-source("HavenAPI.R")
-getPodcastTranscripts("podcastJobIdMap.txt")
-
-readFile(podcastJobIdMap.txt)
-
-episodes <- list.files("Data")
-podcastData <- sapply(episodes, function(x) paste(readLines(paste0("Data/",x), encoding="UTF-8"), collapse=' '))
+podcastData <- read.csv("transcriptions.csv", stringsAsFactors=FALSE)
 
 set.seed(1300)
 
-corpus <- VCorpus(VectorSource(podcastData[9])) %>%
+corpus <- VCorpus(VectorSource(podcastData$transcript)) %>%
   tm_map(x = ., FUN = PlainTextDocument) %>%
   tm_map(x = ., FUN = removePunctuation) %>%
   tm_map(x = ., FUN = removeNumbers) %>%
@@ -28,12 +22,13 @@ corpus <- VCorpus(VectorSource(podcastData[9])) %>%
   tm_map(x = ., FUN = stripWhitespace)
 
 doc_term <- DocumentTermMatrix(corpus)
-doc_term$dimnames$Docs <- c("episode 620", "episode 109")
+doc_term$dimnames$Docs <- podcastData$episode
 
 tf_idf <- weightTfIdf(m = doc_term, normalize = TRUE)
 tf_idf_mat <- as.matrix(tf_idf)
 
 tf_idf_mat
+
 
 
 
